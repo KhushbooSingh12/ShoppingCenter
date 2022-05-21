fac7d978f26d723ad536fa95c3a7413bd47f08c7
khushbooSingh12 <khushboosingh1296@gmail.com>
2022-05-21 16:31:53 +0530

    Printing my name

--- ShoppingCenter/ViewController.swift
+++ ShoppingCenter/ViewController.swift
@@ -11,9 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Khusboo singh")
+        
         // Do any additional setup after loading the view.
     }
-
-
 }
 
